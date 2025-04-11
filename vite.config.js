266ccd4fc249dd79963faa1aac986e039efad6b6
Jacob <program.acquaint533@passinbox.com>
2025-04-11 15:08:36 +1000

    Made the basic site

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +7,17 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (/\.(ttf|woff2?|eot)$/.test(assetInfo.name)) {
+                        return 'fonts/[name][extname]';
+                    }
+                    return 'assets/[name][extname]';
+                },
+            },
+        },
+    },
 });
